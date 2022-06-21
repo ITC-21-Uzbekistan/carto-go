58f96e9cfec4da1e83e231d42b9cae42d602c878
Bekzod <toshpulatovbekki7@icloud.com>
2022-06-21 13:43:19 +0500

    order

--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -8,10 +8,10 @@ from .serializers import OrderSerializer
 class ListCreateOrderView(ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (AllowAny,)
 
 
 class RetrieveUpdateDestroyOrderAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (AllowAny,)
