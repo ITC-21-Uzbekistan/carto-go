bde154e87797fc3f0fe8b5feeb3707469d9345b4
Bekzod <toshpulatovbekki7@icloud.com>
2022-06-21 13:13:08 +0500

    order

--- order/models.py
+++ order/models.py
@@ -4,11 +4,13 @@ from car.models import Car
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=255, default='unknown')
-    email = models.EmailField(default='unknown')
+    name = models.CharField(max_length=255, default=None)
+    email = models.EmailField(default=None)
     phone = models.CharField(max_length=15)
-    city = models.CharField(max_length=100, default='unknown')
+    city = models.CharField(max_length=100, default=None)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='cars')
+    from_time = models.DateField(null=True, default=None)
+    to_time = models.DateField(null=True, default=None)
 
     class Meta:
         db_table = 'order'
